498ab1ed61a5c61b37e84a9d5c1b64fe19705877
Joe Emenaker <joe@emenaker.com>
2021-04-14 10:42:47 -0700

    Cleaned up main code. More suitable for 'main < input.lc > output.js'

--- main.hs
+++ main.hs
@@ -14,29 +14,34 @@ runTests = do
     putStrLn (showExpr (parse "((/ x => (+ x 1)) 8)"))
     putStrLn (showExpr (parse "(/ x => (/ y => (+ x y)))"))
 
+emitLC :: String -> IO ()
+emitLC input = do
+    putStrLn ("LC: " ++ showExpr (parse input))
+
+emitJS :: String -> IO ()
+emitJS input = do
+    putStrLn "// To be run in Node.js"
+    putStrLn "const readline = require(\"readline\");"
+    putStrLn "const r1 = readline.createInterface({"
+    putStrLn "    input: process.stdin,"
+    putStrLn "    output: process.stdout"
+    putStrLn "});"
+    putStrLn "r1.question(\"Please enter a number: \", (input) => {"
+    putStrLn ("    (" ++ showExprJS (parse input) ++ ")(input);")
+    putStrLn "});"
+
+emitPy :: String -> IO ()
+emitPy input = do
+    putStrLn "def println(str):"
+    putStrLn "    print(str)"
+    putStrLn "    return str"
+    putStrLn "print('Please enter a number: '"
+    putStrLn ("(" ++ showExprPy (parse input) ++ ")(input())")
+
 main :: IO ()
 main = do
---     -- The following AST in mainexp should equate to:
---     -- (/ x => println (ifleq0 ((/y => y) 4) 7 x))
---    let mainexp = Abs "x" (Println (Ifleq0 (App (Abs "y" (Id "y")) (Num "4")) (Num "7") (Id "x")))
---    -- Output the AST in LC-ish syntax
---    putStrLn "  ==== The parsed LC sentence is:\n"
---    putStrLn (showExpr mainexp)
---    -- Output the Javascript program that should ask for user input
---    putStrLn "\n  ==== The Javascript version is:\n"
---    putStrLn "const readline = require(\"readline\");"
---    putStrLn "const r1 = readline.createInterface({"
---    putStrLn "    input: process.stdin,"
---    putStrLn "    output: process.stdout"
---    putStrLn "});"
---    putStrLn "r1.question(\"Please enter a number: \", (input) => {"
---    putStrLn ("    (" ++ showExprJS mainexp ++ ")(input);")
---    putStrLn "});"
---    -- Output the Python program that should ask for user input
---    putStrLn "\n  ==== The Python version is:\n"
---    putStrLn "def println(str):"
---    putStrLn "    print(str)"
---    putStrLn "    return str"
---    putStrLn "print('Please enter a number: '"
---    putStrLn ("(" ++ (showExprPy mainexp) ++ ")(input())")
-    runTests
+    input <- getLine
+    -- emitLC input
+    -- emitJS input
+    emitPy input
+    -- runTests
